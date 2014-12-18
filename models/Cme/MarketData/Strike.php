855dbcd1535fea873ba1d0d56b59c15a656b75d5
Vladimir Ivanov <vladimir.ivanov@fastdev.se>
2014-12-18 20:32:19 +0400

    Add volume to strike

--- models/Cme/MarketData/Strike.php
+++ models/Cme/MarketData/Strike.php
@@ -5,16 +5,35 @@ namespace Cme\MarketData;
 
 class Strike extends RowAbstract
 {
+    /**
+     * @var string row type
+     */
     protected $type = 'strike';
 
+    /**
+     * @var int strike
+     */
     protected $strike;
 
+    /**
+     * @var int strike interest rate
+     */
     protected $interest;
+
+    /**
+     * @var int strike volume
+     */
+    protected $volume;
 
     function __construct($row)
     {
-        $this->strike = (int) trim(substr($row, 0, 7));
-        $this->interest = (int) trim(substr($row, 98, 12));
+        $strike   = trim(substr($row, 0, 7));
+        $interest = trim(substr($row, 98, 12));
+        $volume   = trim(substr($row, 86, 12));
+
+        $this->setStrike($strike);
+        $this->setInterest($interest);
+        $this->setVolume($volume);
     }
 
     /**
@@ -31,5 +50,37 @@ class Strike extends RowAbstract
     public function getStrike()
     {
         return $this->strike;
+    }
+
+    /**
+     * @return int
+     */
+    public function getVolume()
+    {
+        return $this->volume;
+    }
+
+    /**
+     * @param int $volume
+     */
+    public function setVolume($volume)
+    {
+        $this->volume = (int) $volume;
+    }
+
+    /**
+     * @param int $interest
+     */
+    public function setInterest($interest)
+    {
+        $this->interest = (int) $interest;
+    }
+
+    /**
+     * @param int $strike
+     */
+    public function setStrike($strike)
+    {
+        $this->strike = (int) $strike;
     }
 }
