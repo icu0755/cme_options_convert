a9b49fd2a461a9e4367e492d61d4c6ca414bd878
Vladimir Ivanov <vladimir.ivanov@fastdev.se>
2015-01-08 21:48:31 +0400

    Remove redundant class static variable

--- models/Cme/Report/ReportDb.php
+++ models/Cme/Report/ReportDb.php
@@ -5,8 +5,6 @@ use Cme\MarketData\Strike;
 
 class ReportDb extends ReportAbstract
 {
-    static public $i = 0;
-
     public function add(Strike $strike)
     {
         $model = new \Cme\Eloquent\Strike($strike->toArray());
